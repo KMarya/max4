ed545ed2b50acdfefec87a1a4bddcaf68e3c3a8d
KMarya <136575725+KMarya@users.noreply.github.com>
2023-08-10 19:22:20 +1000

    Add files via upload

--- advertisements/app/admin.py
+++ advertisements/app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Advertisement
 
 # Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', 'user', 'photo']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description', 'user', 'photo'),
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+        })
+    )
+
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/app/models.py
+++ advertisements/app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+from django.utils import timezone
+from django.utils.html import format_html
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField(
@@ -25,6 +32,28 @@ class Advertisement(models.Model):
         auto_now=True,
         verbose_name='Дата обновления'
     )
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to='advertisements/')
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Изображение')
+    def photo(self):
+        if self.image:
+            return format_html('<img src="%s" width="50" height="50"/>' % self.image.url)
+
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
