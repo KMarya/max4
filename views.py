f55223d271817ce33f3b81881593933b183c1a53
KMarya <136575725+KMarya@users.noreply.github.com>
2023-08-25 18:58:08 +1000

    Delete views.py

--- views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('<h1 style="text-align: center; background-color: yellow">"ДОМАШКА по 4 ЗАНЯТИЮ"')
